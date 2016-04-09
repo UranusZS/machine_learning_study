160ed25d76267fb3383a79117792143118c18ae1
ZS <dragon_201209@126.com>
2016-04-09 19:41:22 +0800

    not finished yet

--- AdaBoost/template/decision_stump_adaboost.h
+++ AdaBoost/template/decision_stump_adaboost.h
@@ -26,28 +26,9 @@ public:
     void init_weak_classifier();
     // learn the weak classifier
     void weak_classifier_learn(DecisionStump & stump);
+    // calculate the sumation of the weights
+    void calculate_weight_sum(double &weight_sum, double &weight_label_sum, double &positive_weight_sum, double &negative_weight_sum);
 
-    // compute the outputs of the weak classifier with precalculated weights
-    bool calculate_classifier_outputs(const double weight_label_sum_right, 
-                                      double &output_right,
-                                      double &output_left,
-                                      const double weight_sum       = 0.0,
-                                      const double weight_label_sum = 0.0,
-                                      const double weight_sum_right = 0.0,
-                                      const double positive_weight_sum_right = 0.0,
-                                      const double negative_weight_sum_right = 0.0,
-                                      const double positive_weight_sum       = 0.0,
-                                      const double negative_weight_sum       = 0.0
-                                );
-
-    // compute the error with precalculated weights, and the outputs of the weak classifier
-    double compute_error(const double positive_weight_sum_right,
-                         const double negative_weight_sum_right,
-                         const double positive_weight_sum_left,
-                         const double negative_weight_sum_left,
-                         const double output_right,
-                         const double output_left
-                    ) const;
 
 private:
     std::vector<DecisionStump> _weak_classifier_vec;
@@ -69,76 +50,29 @@ void DecisionStumpAdaBoost::weak_classifier_learn(DecisionStump &stump) {
     //
 }
 
-bool DecisionStumpAdaBoost::calculate_classifier_outputs(const double weight_label_sum_right, 
-                                                         double &output_right,
-                                                         double &output_left,
-                                                         const double weight_sum,
-                                                         const double weight_label_sum,
-                                                         const double weight_sum_right,
-                                                         const double positive_weight_sum_right,
-                                                         const double negative_weight_sum_right,
-                                                         const double positive_weight_sum,
-                                                         const double negative_weight_sum
-                                                        ) {
-    // different boosting types, require different parameters and return different values
-    switch (this->get_boosting_type()) {
-        case DISCRETE_TYPE:
-            if (weight_label_sum_right > 0) {
-                output_right = 1.0;
-                output_left  = -1.0;
-            } else {
-                output_left  = 1.0;
-                output_right = -1.0;
-            }
-            break;
-        case REAL_TYPE:
-            output_right = log((positive_weight_sum_right + epsilon) / (negative_weight_sum_right + epsilon)) / 2.0;
-            output_left  = log((positive_weight_sum - positive_weight_sum_right + epsilon) / (negative_weight_sum - negative_weight_sum_right + epsilon)) / 2.0;
-            break;
-        case GENTLE_TYPE:
-            output_right = weight_label_sum_right / weight_sum_right;
-            output_left  = (weight_label_sum - weight_label_sum_right) / (weight_sum - weight_sum_right);
-            break;
-        default:
-            return false;
+ 
+void DesisionStumpAdaboost::calculate_weight_sum(double &weight_sum, double &weight_label_sum, double &positive_weight_sum, double &negative_weight_sum) {
+    weight_sum          = 0.0;
+    weight_label_sum    = 0.0;
+    positive_weight_sum = 0.0;
+    negative_weight_sum = 0.0;
+
+    size_t total_sample = this->get_total_sample();
+    for (size_t i = 0; i < total_sample; ++i) {
+        double w = this->get_weight_by_index(i);
+        weight_sum += w;
+        Record record = this->get_sorted_record_by_index(i);
+        if (POSITIVE == record.label) {
+            weight_label_sum    += w;
+            positive_weight_sum += w
+        } else {
+            weight_label_sum    -= w;
+            negative_weight_sum += w;
+        }
     }
-    return true;
 }
 
-double DecisionStumpAdaBoost::compute_error(const double positive_weight_sum_right,
-                                            const double negative_weight_sum_right,
-                                            const double positive_weight_sum_left,
-                                            const double negative_weight_sum_left,
-                                            const double output_right,
-                                            const double output_left
-                                        ) const {
-    // the error 
-    double error = 0.0;
-    switch (this->get_boosting_type()) {
-        case DISCRETE_TYPE:
-            error  = positive_weight_sum_right * (1.0 - output_right) / 2.0;
-            error += positive_weight_sum_left * (1.0 - output_left) / 2.0;
-            error += negative_weight_sum_right * (1.0 + output_right) / 2.0;
-            error += negative_weight_sum_left * (1.0 + output_left) / 2.0;
-            break;
-        case REAL_TYPE:
-            error  = positive_weight_sum_right * exp(-output_right);
-            error += positive_weight_sum_left * exp(-output_left);
-            error += negative_weight_sum_right * exp(output_right);
-            error += negative_weight_sum_left * exp(output_left);
-            break;
-        case GENTLE_TYPE:
-            error  = positive_weight_sum_right * (1.0 - output_right) * (1.0 - output_right);
-            error += positive_weight_sum_left * (1.0 - output_left) * (1.0 - output_left);
-            error += negative_weight_sum_right * (1.0 + output_right) * (1.0 + output_right);
-            error += negative_weight_sum_left * (1.0 + output_left) * (1.0 + output_left);
-            break;
-        default:
-            return error;
-    }
-    return error;
-}
- 
+
 
 
 #endif
